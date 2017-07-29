b572b7387e17991a01b0325ffbebb6e6071aeca1
Prajeet <prajeetpss@gmail.com>
2017-07-29 17:50:52 -0500

    added feature:ignores errors

--- Webscraper/amazon_webscraper.py
+++ Webscraper/amazon_webscraper.py
@@ -2,7 +2,7 @@ from urllib2 import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 
-my_url = 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=macbook'
+my_url = 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=books'
 
 #opens connection by grabbing the page
 uClient = uReq(my_url)
@@ -23,22 +23,16 @@ for container in containers:
 
    # brand_container = container.find_all("a", {"class":"item-brand"})
     #brand = container.div.div.a.img["title"]
-
+   try:
     # the h2 can be replaced with anything to match the tags name....same with the class, can change to be id
-   title_container = container.find_all("h2", {"class": "a-size-medium s-inline s-access-title a-text-normal"})
-   product_name = title_container[0].text
+      title_container = container.find_all("h2", {"class": "a-size-medium s-inline s-access-title a-text-normal"})
+      product_name = title_container[0].text
 
-   price_container = container.find_all("span", {"class": "sx-price-whole"})
-   product_price = price_container[0].text
-   #title_container = container.find_all("a", {"class": "item-title"})
-    #product_name = title_container[0].text
+      price_container = container.find_all("span", {"class": "sx-price-whole"})
+      product_price = price_container[0].text
+      #print("brand: "+ brand)
+      print("Product Name: "+ product_name)
+      print("Price: $"+ product_price)
+   except Exception:
+       pass
 
-    #shipping_container = container.find_all("li", {"class": "price-ship"})
-    #shipping = shipping_container[0].text.strip()
-
-    #print("brand: "+ brand)
-   print("product_name: "+ product_name)
-   print("product_price: $ "+ product_price)
-
-   f.write(product_name.replace(",","|") + "," + product_price +"\n")
-f.close()
